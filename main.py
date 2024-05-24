3816df19bf1c9a1c7e4e88d92796f70e3b8b9f32
tiway <stilon670@gmail.com>
2024-05-24 19:42:48 +0300

    Solution

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import io
+
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    json = JSONRenderer().render(CarSerializer(car).data)
+    return json
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
+    return Car(**data)
